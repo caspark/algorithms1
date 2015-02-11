59fa174a56a38fc4c86ee7d3638f28334f27dec4
Caspar Krieger <ckrieger@atlassian.com>
2015-02-12 09:32:05 +1100

    Only spawn new threads if we need them (branch if jobs > 1)

--- 1-percolation/src/percolation.rs
+++ 1-percolation/src/percolation.rs
@@ -86,31 +86,30 @@ pub fn simulate(n: usize) -> f32 {
 }
 
 pub fn simulate_multiple(n: usize, times: usize, jobs: u32) -> PercolationStats {
-    use std::thread::Thread;
-    use std::sync::{Arc, Mutex, mpsc};
-    use std::cmp;
-
-    // simulation runs left. Data doesn't matter - it's only used to distribute work across jobs.
-    let sims_left = Arc::new(Mutex::new((0..times).collect::<Vec<usize>>()));
-
-    let (tx, rx) = mpsc :: channel();
-    for _ in 0 .. cmp::min(jobs.as_usize(), times) {
-        let tx = tx.clone();
-        let sims_left = sims_left.clone();
-        Thread::spawn(move|| {
-            // acquire lock, fail if another task has failed, try to pop an item, and only continue if we got something
-            while sims_left.lock().unwrap().pop().is_some() {
-                tx.send(simulate(n)).unwrap();
-            }
-        });
-    }
-    let mut results = Vec::with_capacity(times);
-    for _ in 0 .. times {
-        let r = rx.recv().unwrap();
-        results.push(r);
-    }
     PercolationStats {
-        results: results,
+        results: if jobs == 1 {
+            (0..times).map(|_| simulate(n)).collect()
+        } else {
+            use std::thread::Thread;
+            use std::sync::{Arc, Mutex, mpsc};
+            use std::cmp;
+
+            // simulation runs left. Data doesn't matter - it's only used to distribute work across jobs.
+            let sims_left = Arc::new(Mutex::new((0..times).collect::<Vec<usize>>()));
+
+            let (tx, rx) = mpsc :: channel();
+            for _ in 0 .. cmp::min(jobs.as_usize(), times) {
+                let tx = tx.clone();
+                let sims_left = sims_left.clone();
+                Thread::spawn(move|| {
+                    // acquire lock, fail if another task has failed, try to pop an item, and only continue if we got something
+                    while sims_left.lock().unwrap().pop().is_some() {
+                        tx.send(simulate(n)).unwrap();
+                    }
+                });
+            }
+            (0..times).map(|_| rx.recv().unwrap()).collect()
+        },
     }
 }
 
