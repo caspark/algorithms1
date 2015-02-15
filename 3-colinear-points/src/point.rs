627c362b44bea369ee42e4c394bb49b29cbfdcd2
Caspar Krieger <caspar@asparck.com>
2015-02-15 23:04:23 +1100

    Implement slope comparison
    Use i32 instead of u64 because:
    1) "u64 as f64" can lose information
    2) "-2u32 as f64 / -1u32 as f64" gives 1f64 due to precision issues

--- 3-colinear-points/src/point.rs
+++ 3-colinear-points/src/point.rs
@@ -1,18 +1,26 @@
-use std::cmp;
+use std::{cmp, f64};
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Point {
-    x: u64,
-    y: u64,
+    x: i32,
+    y: i32,
 }
 
 impl Point {
-    fn new(x: u64, y: u64) -> Point {
+    fn new(x: i32, y: i32) -> Point {
         Point{x: x, y: y}
     }
 
-    fn slopeTo(that: &Point) -> f64 {
-        panic!("Not implemented yet");
+    fn slope_to(&self, other: &Point) -> f64 {
+        if self == other {
+            f64::NEG_INFINITY
+        } else if self.y == other.y {
+            0f64
+        } else if self.x == other.x {
+            f64::INFINITY
+        } else {
+            ((other.y - self.y) as f64) / ((other.x - self.x) as f64)
+        }
     }
 }
 
@@ -49,6 +57,7 @@ impl cmp::Ord for Point {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::f64;
 
     #[test]
     fn points_ordering_should_be_by_y_coord_then_by_x_coord() {
@@ -64,5 +73,27 @@ mod tests {
         // x should be tie breaker
         assert!(Point::new(1, 1) > Point::new(0, 1));
         assert!(Point::new(0, 1) < Point::new(1, 1));
+    }
+
+    #[test]
+    fn slopes_are_calculated_correctly() {
+        // degenerate line segment
+        assert_eq!(Point::new(0, 0).slope_to(&Point::new(0, 0)), f64::NEG_INFINITY);
+
+        // horizontal line segment
+        assert_eq!(Point::new(0, 0).slope_to(&Point::new(1, 0)), 0f64);
+        assert_eq!(Point::new(1, 0).slope_to(&Point::new(0, 0)), 0f64);
+
+        // vertical line segment
+        assert_eq!(Point::new(0, 0).slope_to(&Point::new(0, 1)), f64::INFINITY);
+        assert_eq!(Point::new(0, 1).slope_to(&Point::new(0, 0)), f64::INFINITY);
+
+        // normal slopes
+        assert_eq!(Point::new(0, 0).slope_to(&Point::new(1, 1)), 1f64);
+        assert_eq!(Point::new(1, 1).slope_to(&Point::new(0, 0)), 1f64);
+        assert_eq!(Point::new(0, 0).slope_to(&Point::new(1, 2)), 2f64);
+        assert_eq!(Point::new(1, 2).slope_to(&Point::new(0, 0)), 2f64);
+        assert_eq!(Point::new(0, 0).slope_to(&Point::new(2, 1)), 0.5f64);
+        assert_eq!(Point::new(2, 1).slope_to(&Point::new(0, 0)), 0.5f64);
     }
 }
