d27aa94b2d4334a0ff175d0e5096d032d4116915
Caspar Krieger <caspar@asparck.com>
2015-02-15 18:51:51 +1100

    Implement subset client

--- 2-queues/src/main.rs
+++ 2-queues/src/main.rs
@@ -5,3 +5,39 @@ extern crate rand;
 mod deque;
 mod linkedlist;
 mod randomizedqueue;
+
+/// Given a command line argument k and n space-separated strings on stdin, randomly print k of those n strings
+fn main() {
+    use std::env;
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Error: missing numeric argument k: the number of items to subset from stdin");
+        return;
+    }
+    match args[1].parse::<u32>() {
+        Ok(k) => {
+            use randomizedqueue::RandomQueue;
+            use std::str;
+            use std::old_io::{self, IoError, IoErrorKind};
+
+            let mut stdin = old_io::stdin();
+            let mut randomq = RandomQueue::new();
+            loop {
+                let read_result = stdin.read_until(' ' as u8);
+                match read_result {
+                    //FIXME better to store strings here than bytes, but couldn't make the lifetimes work
+                    Ok(read_bytes) => randomq.enqueue(read_bytes),
+                    Err(ref err) if err.kind == IoErrorKind::EndOfFile => break,
+                    Err(err) => println!("Error reading from stdin: {}", err),
+                }
+            }
+            for _ in 0 .. k {
+                let e = randomq.dequeue().expect("k must be <= number of strings provided to stdin");
+                let e_as_str = str::from_utf8(&e[]).unwrap();
+                println!("{}", e_as_str.trim());
+            }
+        },
+        Err(_) => println!("Error: received non-numeric k, where k is the number of items to subset from stdin"),
+    }
+}
