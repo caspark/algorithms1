3dfd8c5f64fe3a184c0c320813581409870b52c0
Caspar Krieger <caspar@asparck.com>
2015-02-22 10:18:38 +1100

    Fix up fast co-linear point finding algorithm
    - Include the origin in line segments so we have our 4 points
    - Only report a line segment if the first (sorted) point in it
      is the origin, to avoid reporting the same line segment
      twice in different orders.

--- 3-colinear-points/src/colinearity.rs
+++ 3-colinear-points/src/colinearity.rs
@@ -47,9 +47,6 @@ fn line_from_points(points: &mut [Point]) -> [i32; 4] {
     [points[0].x, points[0].y, points[last].x, points[last].y]
 }
 
-
-
-//FIXME this reports lines of size 3 (instead of 4) but gives the appearance of still working because it reports line subsegments twice
 pub fn find_colinear_points_fast(points: &[Point], line_sender: &Sender<Option<[i32; 4]>>) {
     let mut sortable_points = points.to_vec();
     for origin in points {
@@ -67,28 +64,32 @@ pub fn find_colinear_points_fast(points: &[Point], line_sender: &Sender<Option<[
             while last != sortable_points.len() && slope_to_first == origin.slope_to(&sortable_points[last]) {
                 last += 1;
             }
-            handle_possible_line(first, last, &sortable_points, line_sender);
+            handle_possible_line(&origin, first, last, &sortable_points, line_sender);
 
             first += 1;
         }
     }
 
-    fn handle_possible_line(first: usize, last: usize, sortable_points: &[Point], line_sender: &Sender<Option<[i32; 4]>>) {
-        if last - first >= 3 {
-            let found_line = &sortable_points[first..last];
-
-            let line = {
-                //FIXME this copying approach is clearly a hack
-                let mut v = Vec::with_capacity(last - first);
-                for p in found_line {
-                    v.push(p.clone());
-                }
-                v.sort();
-                let last = v.len() - 1;
-                [v[0].x, v[0].y, v[last].x, v[last].y]
-            };
-            // println!("Sending line {:?} from points {:?}", line, found_line);
-            line_sender.send(Some(line)).unwrap();
+    fn handle_possible_line(origin: &Point, first: usize, last: usize, sortable_points: &[Point], line_sender: &Sender<Option<[i32; 4]>>) {
+        if last - first < 3 {
+            return; // want at least 4 (including origin) points on a line segment
         }
+
+        let line = {
+            //FIXME should be unnecessary to copy here
+            let mut v = Vec::with_capacity(last - first);
+            v.push(origin.clone());
+            for p in &sortable_points[first..last] {
+                v.push(p.clone());
+            }
+            v.sort();
+            if &v[0] != origin {
+                return; // avoid reporting the same line more than once
+            }
+            let last = v.len() - 1;
+            [v[0].x, v[0].y, v[last].x, v[last].y]
+        };
+        // println!("Sending line {:?}", line);
+        line_sender.send(Some(line)).unwrap();
     }
 }
